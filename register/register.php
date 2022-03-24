40af6f917a99a5831e66a0e86a1ab4264d86253c
INGLI BOJA <krymoji@gmail.com>
2022-03-24 20:48:49 +0200

    new start

---
+++ register/register.php
@@ -0,0 +1,27 @@
+<?php
+include('header.php')
+?>
+
+<section id = "reigster">
+    <div class="row m-0">
+        <!-- col-lg-4 4 collums space  -->
+        <div class="col-lg-4 offset-lg-2">
+            <div class="text-center pb-5">
+                <h1 class="login-title text-dark ">Register</h1>
+                <p class="p-1 m-0 font-ubuntu text-black-50">make your register here</p>
+                <span>i already hava a account <a href="login.php">Login</a></span>
+            </div>
+            <div class="upload-profile-image">
+                <div class="text-center">
+                    <div class="d-flex justify-content-center">
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include('footer.php')
+?>
